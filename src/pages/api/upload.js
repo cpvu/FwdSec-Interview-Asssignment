a7e1f50122205826521b121e6d4f2f4411de09f5
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2023-04-27 02:27:08 -0700

    Refactor functions and add coverage calculation

--- src/pages/api/upload.js
+++ src/pages/api/upload.js
@@ -2,6 +2,7 @@
 import formidable from "formidable";
 import fs from "fs";
 import xml2js from "xml2js";
+import lodash from "lodash";
 
 export const config = {
   api: {
@@ -9,49 +10,112 @@ export const config = {
   },
 };
 
+//Parse the formData object sent from the client containing both the burp suite history file and swagger file
+async function parseFormData(request) {
+  return new Promise((resolve, reject) => {
+    const form = formidable({ multiples: true, maxFileSize: 50 * 1024 * 1024 });
+
+    form.parse(request, async (err, fields, files) => {
+      if (err) {
+        console.log(err);
+        reject({ success: false });
+      } else {
+        const burpSuiteFile = files.burpSuiteHistoryFile;
+        const swaggerFile = files.swaggerFile;
+
+        resolve([burpSuiteFile, swaggerFile]);
+      }
+    });
+  });
+}
+
+//Convert the JSON persistent file into a JSON
+async function fileJsonDataToJson(jsonFile) {
+  const jsonContent = await fs.promises.readFile(jsonFile.filepath);
+  const jsonData = JSON.parse(jsonContent);
+
+  return jsonData;
+}
+
+//Convert the XML persistent file into a JSON
+async function fileXMLDataToJson(xmlFile) {
+  const parser = new xml2js.Parser();
+
+  const fileReadStream = fs.readFileSync(xmlFile.filepath);
+
+  let xmlJSON = new Promise((resolve, reject) => {
+    parser.parseString(fileReadStream, (err, result) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      }
+      // Convert the JavaScript object to a JSON string
+      resolve(JSON.parse(JSON.stringify(result)));
+    });
+  });
+
+  return xmlJSON;
+}
+
+async function calculateCoverage(swaggerJSON, burpSuiteHistoryJSON) {
+  let hashmap = {};
+  let number = 0;
+  let responseJson = {};
+
+  let swaggerEndpoints = Object.keys(swaggerJSON.paths);
+
+  swaggerEndpoints.forEach((route) => {
+    let endPointMethods = Object.keys(swaggerJSON.paths[`${route}`]);
+    number += endPointMethods.length;
+
+    if (!hashmap[route]) {
+      hashmap[route] = endPointMethods;
+    }
+  });
+
+  //console.log(hashmap);
+
+  burpSuiteHistoryJSON.items.item.forEach((item) => {
+    let burpMethod = item.method.toString().toLowerCase();
+    let burpPath = item.path.toString().substring(3);
+
+    console.log(burpPath.split("?")[0]);
+
+    for (let endpoint of swaggerEndpoints) {
+      const pattern = endpoint.replace(/\{\w+\}/g, "\\w+");
+      if (
+        new RegExp(`^${pattern}$`).test(burpPath.split("?")[0]) &&
+        hashmap[endpoint].includes(burpMethod)
+      ) {
+        console.log(
+          `Endpoint '${endpoint}' matched key '${burpPath}' with methods ${hashmap[endpoint]}`
+        );
+
+        hashmap[endpoint].pop(burpMethod);
+        break;
+      }
+    }
+  });
+
+  //console.log(hashmap);
+  //console.log(number);
+  //console.log(responseJson);
+}
+
+//Route request handler
 export default async function handler(req, res) {
   // If user authentication is added, can add a check for ensure route is protected
   if (req.method != "POST") {
     return res.status(400).json({ success: "false" });
   }
-
-  const form = formidable({ multiples: true, maxFileSize: 50 * 1024 * 1024 });
-
   try {
-    form.parse(req, async (err, field, files) => {
-      if (err) {
-        console.log(err);
-        return res.status(401).json({ success: false });
-      }
-      console.log("Parsing Files...");
+    const [burpSuiteHistoryFile, swaggerFile] = await parseFormData(req);
+    const swaggerJSON = await fileJsonDataToJson(swaggerFile);
+    const burpSuiteHistoryJSON = await fileXMLDataToJson(burpSuiteHistoryFile);
 
-      const burpSuiteHistoryFile = files.burpSuiteHistoryFile;
-      const swaggerFile = files.swaggerFile;
+    calculateCoverage(swaggerJSON, burpSuiteHistoryJSON);
 
-      const jsonContent = await fs.promises.readFile(swaggerFile.filepath);
-      const jsonData = JSON.parse(jsonContent);
-
-      const fileReadStream = fs.readFileSync(burpSuiteHistoryFile.filepath);
-
-      // Parse the XML stream into a JavaScript object
-      const parser = new xml2js.Parser();
-
-      let string = "";
-
-      parser.parseString(fileReadStream, (err, result) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        // Convert the JavaScript object to a JSON string
-        string += JSON.stringify(result);
-      });
-
-      console.log(string);
-      console.log(JSON.parse(string));
-
-      return res.status(200).json({ success: true });
-    });
+    return res.status(200).json({ success: true });
   } catch (err) {
     console.log(err);
   }
