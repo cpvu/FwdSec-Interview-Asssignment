11c48d18f4b32f6eb41188612ecca9eedfce7dc2
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2023-04-29 02:34:22 -0700

    Add coverage report store

--- src/states/store.js
+++ src/states/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch, useSelector } from "react-redux";
 import { uploadedFilesSlice } from "./fileUpload/";
+import { coverageReportSlice } from "./coverageReport/coverageReportSlice";
 
 export const store = configureStore({
   reducer: {
+    coverageReport: coverageReportSlice.reducer,
     uploadedFiles: uploadedFilesSlice.reducer,
   },
 });
