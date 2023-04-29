a3706bbdd7d4ac1ceadae9a02b5ad0db297d8d86
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2023-04-28 17:06:52 -0700

    Add styling and modify text

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
+import styles from "src/styles/Header.module.css";
+
 export const Header = () => {
   return (
     <div>
-      <header>Fwd Sec Assignment</header>
+      <header className={styles.mainHeader}>
+        Forward Security Interview Assignment by Calvin Vu
+      </header>
     </div>
   );
 };
