93f6b55610dcf8b8b56f12f0647f24505e49845e
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2023-04-29 13:30:18 -0700

    Add coverage report page

--- src/pages/coverageReport.js
+++ src/pages/coverageReport.js
@@ -1,10 +1,73 @@
-import { useContext } from "react";
+import { useSelector } from "react-redux";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { resetCoverageReport } from "@/states/coverageReport/coverageReportSlice";
+import { resetFiles } from "@/states/fileUpload/uploadedFilesSlice";
 
 export default function coverageReport() {
-  useContext();
+  const router = useRouter();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    return () => {
+      dispatch(resetCoverageReport());
+      dispatch(resetFiles());
+    };
+  }, []);
+
+  let coverageReport = useSelector(
+    (state) => state.coverageReport.coverageReport
+  );
+
   return (
     <div>
-      <h1>hello</h1>
+      <header>Result:</header>
+      {coverageReport ? (
+        <div>
+          <p>Total Endpoints: {coverageReport.totalEndpoints}</p>
+          <p>Coverage: {coverageReport.coverage}%</p>
+          <table className="coverageTable">
+            <tr>
+              <th>UntestedEndPoints</th>
+              <th>Methods</th>
+            </tr>
+            {Object.keys(coverageReport.untestedEndpoints).map((endpoint) => {
+              return (
+                <tr>
+                  <td key={endpoint}>{endpoint}</td>
+                  <td key={coverageReport.untestedEndpoints[`${endpoint}`]}>
+                    {coverageReport.untestedEndpoints[`${endpoint}`].toString()}
+                  </td>
+                </tr>
+              );
+            })}
+          </table>
+
+          <table className="coverageTable">
+            <tr>
+              <th>TestedEndpoints</th>
+              <th>Methods</th>
+            </tr>
+            {Object.keys(coverageReport.testedEndpoints).map((endpoint) => {
+              return (
+                <tr>
+                  <td>{endpoint}</td>
+                  <td>
+                    {coverageReport.testedEndpoints[`${endpoint}`].toString()}
+                  </td>
+                </tr>
+              );
+            })}
+          </table>
+        </div>
+      ) : (
+        <label onLoad={returnHome}>Returning home..</label>
+      )}
+      <br></br>
+      <button className="submitUpload" onClick={() => router.push("/")}>
+        Return home
+      </button>
     </div>
   );
 }
