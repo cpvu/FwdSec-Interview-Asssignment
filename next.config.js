4b376faaad30617f010fd3f70f6ccf9db15a0d24
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2023-05-01 01:43:49 -0700

    Add documentation

--- next.config.js
+++ next.config.js
@@ -9,18 +9,22 @@ const ContentSecurityPolicy = `
 `;
 
 const securityHeaders = [
+  //Browsers can only be accessed via https
   {
     key: "Strict-Transport-Security",
     value: "max-age=63072000;",
   },
+  //Header stops browser from loading if XSS detected
   {
     key: "X-XSS-Protection",
     value: "1; mode=block",
   },
+  //Prevent XSS exploits for file sharing/uploading
   {
     key: "X-Content-Type-Options",
     value: "nosniff",
   },
+  //Prevent XSS, click jacking, and injection attacks
   {
     key: "Content-Security-Policy",
     value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
@@ -29,7 +33,6 @@ const securityHeaders = [
 
 const nextConfig = {
   reactStrictMode: true,
-
   async headers() {
     return [
       {
